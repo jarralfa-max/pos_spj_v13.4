ea828a6c5331ccaa43e6d6b3944df5411e4bca2e
jarralfa-max <jarr.alfa@gmail.com>
2026-06-03 14:06:55 -0600

    mover forntend, backend y tests a la arquitectura correcta

--- tests/architecture/test_no_commit_rollback_in_frontend.py
+++
@@ -1,7 +0,0 @@
-from .allowlists import COMMIT_ROLLBACK_IN_UI_ALLOWLIST
-from .architecture_guardrails import COMMIT_ROLLBACK_RE, UI_ROOTS, assert_no_new_violations, collect_regex_violations
-
-
-def test_no_commit_rollback_in_frontend() -> None:
-    violations = collect_regex_violations(pattern=COMMIT_ROLLBACK_RE, roots=UI_ROOTS)
-    assert_no_new_violations("commit/rollback in UI", violations, COMMIT_ROLLBACK_IN_UI_ALLOWLIST)
--- tests/integration/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Integration test package for the SPJ refactor phases."""
